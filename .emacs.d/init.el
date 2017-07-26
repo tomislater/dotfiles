b26ba11e9e3ea30ba29d8e0a00948107c1545817
Tomasz Święcicki <tswiecicki@opera.com>
2017-07-26 17:57:50 +0200

    Add support for emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,29 @@
+(require 'package) ;; You might already have this line
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(package-selected-packages
+   (quote
+    (fiplr yaml-mode flycheck-yamllint elpy neotree js2-refactor js2-highlight-vars ac-js2 projectile markdown-mode+ markdown-mode dockerfile-mode docker flycheck-cython flycheck magit-gitflow magit-find-file magit-filenotify magit solarized-theme which-key terraform-mode hcl-mode git-blamed csv-nav csv-mode csv auto-complete-rst ansible xkcd))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(global-linum-mode t)
+(setq fiplr-ignored-globs '((directories (".git" ".svn"))
+                            (files ("*~"))))
