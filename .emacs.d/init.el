26e658dac3c0488013e6cbe57f0b4dd772cc3418
Tomek Święcicki <tomislater@gmail.com>
2017-07-26 20:43:34 +0200

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,18 +12,27 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(package-selected-packages
    (quote
-    (fiplr yaml-mode flycheck-yamllint elpy neotree js2-refactor js2-highlight-vars ac-js2 projectile markdown-mode+ markdown-mode dockerfile-mode docker flycheck-cython flycheck magit-gitflow magit-find-file magit-filenotify magit solarized-theme which-key terraform-mode hcl-mode git-blamed csv-nav csv-mode csv auto-complete-rst ansible xkcd))))
+    (fiplr yaml-mode elpy neotree all-the-icons js2-refactor js2-highlight-vars ac-js2 projectile markdown-mode dockerfile-mode flycheck magit-find-file magit-filenotify magit which-key terraform-mode hcl-mode git-blamed csv-nav csv-mode csv auto-complete-rst ansible))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
 (global-linum-mode t)
+
+;; what fiplr should ignore
 (setq fiplr-ignored-globs '((directories (".git" ".svn"))
                             (files ("*~"))))
+
+;; elpy
+(elpy-enable)
+
+;; neotree
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+
+;; which-key
+(which-key-mode)
